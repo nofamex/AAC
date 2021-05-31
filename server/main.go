504248e38016faebaa06c74fc0f9d8caa6daaab0
Mustafa Zaki Assagaf <mustafa.segf@gmail.com>
2021-05-31 18:06:08 +0700

    feat: create server init

--- server/main.go
+++ server/main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+
+	"github.com/nofamex/AAC/server/util"
+	"github.com/nofamex/AAC/server/api"
+)
 
 func main() {
+	config, err := util.LoadConfig("../")
+	if err != nil {
+		log.Fatal("cannot load config:", err)
+	}
+	
+	server, err := api.NewServer(config)
+	err = server.StartServer(config.Port)
+	if err != nil {
+		log.Fatal("cannot start server:", err)
+	}
+	
 	fmt.Println("Starting")
 }
