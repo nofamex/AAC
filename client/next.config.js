56188229a8487d7fb2a25541803e5d08bd8b6cc2
nofamex <nsiren517@gmail.com>
2021-06-28 19:31:33 +0700

    feat: change the client architecture

--- client/next.config.js
+++ client/next.config.js
@@ -1,8 +1,3 @@
 module.exports = {
-    webpack: (config, { isServer }) => {
-      if (!isServer) {
-        config.node = { fs: 'empty', module: 'empty' }
-      }
-      return config
-    },
-  }
+  reactStrictMode: true,
+}
