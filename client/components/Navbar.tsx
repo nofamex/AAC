3a84c9e8f3e54a57a99c0bb548520716033bccb2
nofamex <nsiren517@gmail.com>
2021-07-01 13:00:29 +0700

    feat: fix navbar and make layout

--- client/components/Navbar.tsx
+++ client/components/Navbar.tsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 import Image from "next/image";
 import Button from "./Button";
 import NavItem from "./NavItem";
+import { slide as Menu } from "react-burger-menu";
+import { GiHamburgerMenu } from "react-icons/gi";
 
 export default function Navbar() {
   const [selectedMenu, setSelectedMenu] = useState("Home");
@@ -13,9 +15,9 @@ export default function Navbar() {
   };
 
   return (
-    <div className="h-16 bg-black-80 flex items-center px-nav">
+    <div className="h-16 bg-black-80 flex items-center px-6 md:px-nav">
       <Image src="/picture/logo.svg" alt="AAC Logo" width="80" height="58" />
-      <div className="ml-auto h-16 flex items-center">
+      <div className="ml-auto h-16 hidden md:flex items-center">
         {menus.map((menu, index) => (
           <NavItem
             key={index}
@@ -25,6 +27,16 @@ export default function Navbar() {
           />
         ))}
         <Button text="Sign In" handler={signInHandler} filled={false} />
+      </div>
+      <div className="ml-auto md:ml-0">
+        <Menu
+          customBurgerIcon={<GiHamburgerMenu className="text-white" />}
+          burgerButtonClassName="w-7 h-7 block md:hidden"
+        >
+          <a href="">Home</a>
+          <a href="">Competition</a>
+          <a href="">Seminar</a>
+        </Menu>
       </div>
     </div>
   );
