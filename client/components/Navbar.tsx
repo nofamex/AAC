0c314d1aef87b720005fe932ba6047e7f3dad2c5
nofamex <nsiren517@gmail.com>
2021-07-06 05:33:54 +0700

    feat: add animation layout and mobile

--- client/components/Navbar.tsx
+++ client/components/Navbar.tsx
@@ -1,7 +1,11 @@
 import NavbarDesktop from "./NavbarDesktop";
 import NavbarMobile from "./NavbarMobile";
 
-export default function Navbar() {
+interface NavbarProps {
+  scroll: boolean;
+}
+
+export default function Navbar({ scroll }: NavbarProps) {
   const menus = [
     { page: "/", text: "Home" },
     { page: "/competition", text: "Competition" },
@@ -13,7 +17,11 @@ export default function Navbar() {
   };
 
   return (
-    <div className="h-16 bg-black-80 flex items-center px-6 md:px-nav w-full fixed z-50">
+    <div
+      className={`h-16 bg-black-80 ${
+        scroll ? "md:bg-black-80" : "md:bg-transparent"
+      } flex items-center px-6 md:px-nav w-full fixed z-50 transition-all`}
+    >
       <NavbarDesktop handler={signInHandler} menus={menus} />
       <NavbarMobile handler={signInHandler} menus={menus} />
     </div>
--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,10 +1,27 @@
+// @ts-nocheck
 import HeaderVideo from "../components/HeaderVideo";
 import Layout from "../components/Layout";
+import { useState } from "react";
+import { useScrollPosition } from "../hooks/useScrollPostion";
+import HeaderMobile from "../components/HeaderMobile";
 
 export default function Home() {
+  const [scroll, setScroll] = useState(false);
+
+  useScrollPosition(
+    ({ currPos }: any) => {
+      const isShow = currPos.y < 0;
+
+      if (isShow !== scroll) setScroll(isShow);
+      else setScroll(isShow);
+    },
+    [scroll]
+  );
+
   return (
     <Layout>
-      <HeaderVideo />
+      <HeaderVideo scroll={scroll} />
+      <HeaderMobile />
     </Layout>
   );
 }
