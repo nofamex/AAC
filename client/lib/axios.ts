0fca72e4f903a61c0a3f8ce119f98db8240a6f51
nofamex <nsiren517@gmail.com>
2021-07-12 18:39:22 +0700

    semoga jalan

--- client/lib/axios.ts
+++ client/lib/axios.ts
@@ -1,9 +1,13 @@
 import axios from "axios";
 import https from "https";
 import { getAccessToken } from "./auth";
+const token = getAccessToken();
+console.log(token);
 const api = axios.create({
   baseURL: "https://18.141.81.252/api/v1",
   httpsAgent: new https.Agent({ rejectUnauthorized: false }),
-  headers: { Authorization: `Bearer ${getAccessToken || ""}` },
+  headers: {
+    Authorization: `Bearer ${typeof token === "undefined" ? "" : token}`,
+  },
 });
 export default api;
