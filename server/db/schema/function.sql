e807a8fb8fa6028f8b3a5090dc14f6b506a33abe
Mustafa Zaki Assagaf <mustafa.segf@gmail.com>
2021-08-24 00:35:37 +0700

    feat: fix procedure

--- server/db/schema/function.sql
+++ server/db/schema/function.sql
@@ -87,9 +87,25 @@ $$
           update prelim_tac_master
           set benar = benar + 1
           where team_id = new.team_id;
-        else
+          
+          update prelim_tac_master
+          set salah = salah - 1
+          where team_id = new.team_id;
+          
+          update prelim_tac_master
+          set score = score + 2*(select bobot from prelim_tac_pg where id = new.soal_id) + 2
+          where team_id = new.team_id;
+        elsif old.jawaban = (select jawaban from prelim_tac_pg where id=new.soal_id) then
+          update prelim_tac_master
+          set benar = benar - 1
+          where team_id = new.team_id;
+          
           update prelim_tac_master
           set salah = salah + 1
+          where team_id = new.team_id;
+          
+          update prelim_tac_master
+          set score = score - 2*(select bobot from prelim_tac_pg where id = new.soal_id) - 2
           where team_id = new.team_id;
         end if;
       end if;
@@ -104,3 +120,142 @@ after insert or update or delete on prelim_tac_pg_jawaban
 for each row
 execute PROCEDURE score_tac_pg();
 
+
+
+
+
+create or replace function score_unac_pg()
+returns trigger as
+$$
+  begin
+    if (TG_OP = 'INSERT') then 
+      update prelim_unac_master
+      set kosong = kosong -1
+      where team_id = new.team_id;
+
+      if new.jawaban = (select jawaban from prelim_unac_pg where id=new.soal_id) then
+        update prelim_unac_master
+        set benar = benar + 1
+        where team_id = new.team_id;
+
+        update prelim_unac_master
+        set score = score + (select bobot from prelim_unac_pg where id = new.soal_id) + 2
+        where team_id = new.team_id;
+      else
+        update prelim_unac_master
+        set salah = salah + 1
+        where team_id = new.team_id;
+
+        update prelim_unac_master
+        set score = score - (select bobot from prelim_unac_pg where id = new.soal_id)
+        where team_id = new.team_id;
+      end if;
+    end if;
+    if (TG_OP = 'UPDATE') then
+      if (OLD.jawaban <> NEW.jawaban) then
+        if new.jawaban = (select jawaban from prelim_unac_pg where id=new.soal_id) then
+          update prelim_unac_master
+          set benar = benar + 1
+          where team_id = new.team_id;
+          
+          update prelim_unac_master
+          set salah = salah - 1
+          where team_id = new.team_id;
+          
+          update prelim_unac_master
+          set score = score + 2*(select bobot from prelim_unac_pg where id = new.soal_id) + 2
+          where team_id = new.team_id;
+        elsif old.jawaban = (select jawaban from prelim_unac_pg where id=new.soal_id) then
+          update prelim_unac_master
+          set benar = benar - 1
+          where team_id = new.team_id;
+          
+          update prelim_unac_master
+          set salah = salah + 1
+          where team_id = new.team_id;
+          
+          update prelim_unac_master
+          set score = score - 2*(select bobot from prelim_unac_pg where id = new.soal_id) - 2
+          where team_id = new.team_id;
+        end if;
+      end if;
+    end if;
+    return new;
+  end;
+$$
+LANGUAGE plpgsql;
+
+create trigger trigger_score_unac_pg
+after insert or update or delete on prelim_unac_pg_jawaban
+for each row
+execute PROCEDURE score_unac_pg();
+
+
+
+
+
+create or replace function score_unac_isian()
+returns trigger as
+$$
+  begin
+    if (TG_OP = 'INSERT') then 
+      update prelim_unac_master
+      set kosong = kosong -1
+      where team_id = new.team_id;
+
+      if new.jawaban in (select regexp_split_to_table(LEFT(RIGHT(jawaban, LENGTH(jawaban) - 3), LENGTH(jawaban) - 7), ';') from prelim_unac_isian where id=new.soal_id) then
+        update prelim_unac_master
+        set benar = benar + 1
+        where team_id = new.team_id;
+
+        update prelim_unac_master
+        set score = score + 2*(select bobot from prelim_unac_isian where id = new.soal_id) + 2
+        where team_id = new.team_id;
+      else
+        update prelim_unac_master
+        set salah = salah + 1
+        where team_id = new.team_id;
+
+        update prelim_unac_master
+        set score = score - (select bobot from prelim_unac_isian where id = new.soal_id) - 1
+        where team_id = new.team_id;
+      end if;
+    end if;
+    if (TG_OP = 'UPDATE') then
+      if (OLD.jawaban <> NEW.jawaban) then
+        if new.jawaban in (select regexp_split_to_table(LEFT(RIGHT(jawaban, LENGTH(jawaban) - 3), LENGTH(jawaban) - 7), ';') from prelim_unac_isian where id=new.soal_id)  then
+          update prelim_unac_master
+          set benar = benar + 1
+          where team_id = new.team_id;
+          
+          update prelim_unac_master
+          set salah = salah - 1
+          where team_id = new.team_id;
+          
+          update prelim_unac_master
+          set score = score + 3*(select bobot from prelim_unac_isian where id = new.soal_id) + 3
+          where team_id = new.team_id;
+        elsif old.jawaban in (select regexp_split_to_table(LEFT(RIGHT(jawaban, LENGTH(jawaban) - 3), LENGTH(jawaban) - 7), ';') from prelim_unac_isian where id=new.soal_id)  then
+          update prelim_unac_master
+          set benar = benar - 1
+          where team_id = new.team_id;
+          
+          update prelim_unac_master
+          set salah = salah + 1
+          where team_id = new.team_id;
+          
+          update prelim_unac_master
+          set score = score - 3*(select bobot from prelim_unac_isian where id = new.soal_id) - 3
+          where team_id = new.team_id;
+        end if;
+      end if;
+    end if;
+    return new;
+  end;
+$$
+LANGUAGE plpgsql;
+
+create trigger trigger_score_unac_isian
+after insert or update or delete on prelim_unac_isan_jawaban
+for each row
+execute PROCEDURE score_unac_isian();
