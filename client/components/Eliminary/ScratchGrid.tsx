bc6e844445652e1e22d89543b39f221c455b084a
nofamex <nsiren517@gmail.com>
2021-08-24 17:19:01 +0700

    finish scratch

--- client/components/Eliminary/ScratchGrid.tsx
+++ client/components/Eliminary/ScratchGrid.tsx
@@ -1,30 +1,50 @@
 import { useState } from "react";
+import { wordArr } from "@lib/word";
 
 export default function ScratchGrid({ generated }: any) {
   const wordsInstance = generated.instance;
   const [firstWord, setFirstWord] = useState<any>({});
   const [lastWord, setLastWord] = useState<any>({});
   const [selectedWord, setSelectedWord] = useState<any>([]);
+  const [findedWord, setFindedWord] = useState<any>([]);
+
+  const refreshSelected = () => {
+    setFirstWord({});
+    setLastWord({});
+    setSelectedWord([]);
+  };
 
   const wordClickHandler = (x: number, y: number) => {
-    const firstWordLength = Object.entries(firstWord).length;
-
-    if (firstWordLength === 0) {
+    if (Object.entries(firstWord).length === 0) {
       setFirstWord({ x, y });
+      setLastWord({ x, y });
       selectedWord.push(`x-${x},y-${y}`);
     } else {
       if (wordsInstance.read(firstWord, { x, y }) === null) {
-        setFirstWord({});
-        setLastWord({});
-        setSelectedWord([]);
+        refreshSelected();
       } else {
         setLastWord({ x, y });
-        selectedWord.push(`x-${x},y-${y}`);
+        let newSelectedWordArr: string[] = [];
+        wordsInstance.utils
+          .createPathFromPair(firstWord, { x, y })
+          .forEach((item: { x: number; y: number }) => {
+            newSelectedWordArr.push(`x-${item.x},y-${item.y}`);
+          });
+        setSelectedWord(newSelectedWordArr);
+        findWordChecker(x, y, newSelectedWordArr);
       }
     }
   };
 
-  console.log(wordsInstance.read(firstWord, lastWord));
+  const findWordChecker = (x: number, y: number, selectedWord: string[]) => {
+    if (wordArr.includes(wordsInstance.read(firstWord, { x, y }))) {
+      setFindedWord([...selectedWord, ...findedWord, `x-${x},y-${y}`]);
+      refreshSelected();
+      console.log(
+        `Berhasil Menemukan ${wordsInstance.read(firstWord, { x, y })}`
+      );
+    }
+  };
 
   return (
     <div className="h-full w-11/12 rounded-xl bg-compe ml-4 p-4 text-white font-md font-bold flex flex-col justify-center items-center">
@@ -40,6 +60,7 @@ export default function ScratchGrid({ generated }: any) {
               firstWord={firstWord}
               ws={generated.instance}
               selectedWord={selectedWord}
+              findedWord={findedWord}
             />
           ))}
         </div>
@@ -56,18 +77,30 @@ interface WordProps {
   firstWord: { x: number; y: number };
   ws: any;
   selectedWord: string[];
+  findedWord: string[];
 }
 
-function Word({ x, y, ct, handler, firstWord, ws, selectedWord }: WordProps) {
+function Word({
+  x,
+  y,
+  ct,
+  handler,
+  firstWord,
+  ws,
+  selectedWord,
+  findedWord,
+}: WordProps) {
   const isSelected =
     ws.read(firstWord, { x, y }) !== null &&
     selectedWord.includes(`x-${x},y-${y}`);
+
+  const isFinded = findedWord.includes(`x-${x},y-${y}`);
 
   return (
     <div
       className={`h-full w-full flex justify-center items-center cursor-pointer ${
         isSelected && "bg-orange"
-      }`}
+      } ${isFinded && "bg-green-500"}`}
       onClick={() => handler(x, y)}
     >
       {ct}
