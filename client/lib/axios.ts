eae879d7253b5ed81969853ec813c4081ee65c71
nofamex <nsiren517@gmail.com>
2021-09-06 21:11:39 +0700

    change api

--- client/lib/axios.ts
+++ client/lib/axios.ts
@@ -5,7 +5,7 @@ import { getAccessToken } from "./auth";
 const token = getAccessToken();
 
 const api = axios.create({
-  baseURL: "https://be75-103-3-220-175.ngrok.io/api/v1",
+  baseURL: "https://aacunair.ddns.net/api/v1",
   httpsAgent: new https.Agent({ rejectUnauthorized: false }),
   headers: {
     Authorization: `Bearer ${typeof token === "undefined" ? "" : token}`,
